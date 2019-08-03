c659a7af1cdf580a50e18136f6381d54250486b4
a <ruan65@gmail.com>
2019-08-03 17:30:39 +0300

    initial

---
+++ ios/DerivedDataBuild/Runner/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/a/dev/Flutter_apps/gif_smth/ios/DerivedDataBuild/Runner/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
